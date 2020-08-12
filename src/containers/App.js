9dc3bdd1b147c8acc69968be6663ec79b6cc43ca
Vane Spasov <Vane.Spasov@endava.com>
2020-08-12 17:06:55 +0200

    Added all files.

---
+++ src/containers/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons'
+
+class App extends Component{  
+  
+  state = {
+    persons: [
+      {name:"Max", age:28, id:1},
+      {name:"Manu", age:29, id:2},
+      {name:"Stephanie", age:26, id:3}
+    ],
+    otherState:'some other state',
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {return p.id===id});
+    const person={
+      ...this.state.persons[personIndex]
+    };
+    person.name=event.target.value;
+    const persons = [...this.state.persons]
+    persons[personIndex] = person;
+
+    this.setState({ persons:persons });
+  };
+
+  togglePersonsHandler = () =>{
+    const oldState= this.state.showPersons;
+    this.setState({showPersons:!oldState})
+  }
+
+  deletePersonHandler = (personIndex) =>{
+    const persons=this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  render () {    
+    let persons = null;
+    let btnClass = '';
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          <Persons 
+            persons = {this.state.persons}
+            clicked = {this.deletePersonHandler} 
+            changed = {this.nameChangedHandler}/>          
+        </div>
+      );
+      
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+    }
+
+      return (
+          <div className={classes.App}>
+            <h1>Hi, I'm a React App</h1>
+            <p className={assignedClasses.join(' ')}>This is really working.</p>
+            <button className={btnClass} onClick={this.togglePersonsHandler}>
+              Toggle Persons
+            </button>
+            {persons}
+          </div>
+      );
+    }
+}
+
+export default App;
